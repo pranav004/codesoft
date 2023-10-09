098191743a149c490579912fb6adf520ef29e557
pranav004 <89502291+pranav004@users.noreply.github.com>
2023-10-09 20:32:39 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,42 @@
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Get user input for two numbers and operation choice
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Choose an operation (+, -, *, /): ")
+
+        if operation not in ('+', '-', '*', '/'):
+            print("Invalid operation! Please choose +, -, *, or /.")
+        else:
+            if operation == '+':
+                result = add(num1, num2)
+            elif operation == '-':
+                result = subtract(num1, num2)
+            elif operation == '*':
+                result = multiply(num1, num2)
+            elif operation == '/':
+                result = divide(num1, num2)
+
+            print("Result: ", result)
+            break
+    except ValueError:
+        print("Invalid input! Please enter valid numbers.")
+
